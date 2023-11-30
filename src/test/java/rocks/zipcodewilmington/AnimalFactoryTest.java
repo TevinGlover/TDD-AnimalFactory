d02875c667bc0041ff8acd196a86301b159556d0
Tevin Glover <tevwork.tg@gmail.com>
2023-11-30 09:05:11 -0500

    done

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
 import java.util.Date;
 
@@ -18,22 +19,40 @@ public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     @Test
     public void testAnimalCreateDog(){
+      Dog newDog =  AnimalFactory.createDog("Lion", new Date(2023,04,12));
 
 
-
-
-     //   Assert.assertNotNull();
-
+        Date   actualDOB = new Date(2023,04,12);
+        Date   expectedDOB = newDog.getBirthDate();
+        String  actualName = "Lion";
+        String  expectedName = newDog.getName();
+        Assert.assertNotNull(newDog);
+        Assert.assertEquals(expectedDOB,actualDOB);
+        Assert.assertEquals(expectedName,actualName);
+        Assert.assertNotNull(newDog.getId());// how dos it create a Id??
+        System.out.println(newDog.getId() + " this is the dog id");// i wonder how this works.
     }
 
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
     @Test
     public void testAnimalCreateCat(){
+        Cat newCat = AnimalFactory.createCat("Wolf", new Date(2014,04,12));
 
 
 
+        Date   actualDOB = new Date(2014,04,12);
+        Date   expectedDOB = newCat.getBirthDate();
 
-       // Assert.assertNotNull();
+        String  actualName = "Wolf";
+        String  expectedName = newCat.getName();
+        Assert.assertNotNull(newCat.getId());
+        Assert.assertEquals(expectedDOB,actualDOB);
+        Assert.assertEquals(expectedName,actualName);
+       Assert.assertNotNull(newCat);
+        Assert.assertNotNull(newCat.getId());// how dos it create a Id??
+        System.out.println(newCat.getId() + " this is the cat id");// i wonder how this works.
+        // i understand now the id is created  by what order the Animal factor creates them or
+        // the order   their place in the house dog house. nice.
 
     }
 }
